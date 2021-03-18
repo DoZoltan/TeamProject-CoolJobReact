07fcbd2cb438de0457d2387c55a6a527142042ff
Roland Horváth <rolandhorvath1025@gmail.com>
2021-03-18 15:24:40 +0100

    change background

--- src/components/mainPage/Main.js
+++ src/components/mainPage/Main.js
@@ -1,29 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
-import logo from '../../resources/logo.jpg';
+import background from '../../resources/background.png';
 
 const LogoDiv = styled.div`
 	background-size: cover;
 	background-position: center center;
 	background-repeat: no-repeat;
-	background-image: url(${logo});
+	background-image: url(${background});
 	width: 100%;
 	height: 100vh;
 `;
 
-const P = styled.p`
-	color: orange;
-	padding: 20px;
-	text-align: center;
-	font-size: 3.5vw;
-`;
+// const P = styled.p`
+// 	color: orange;
+// 	padding: 20px;
+// 	text-align: center;
+// 	font-size: 3.5vw;
+// `;
 
 export const Main = () => {
 	return (
 		<div>
-			<LogoDiv>
-				<P id={'welcomeText'}>Welcome to the Cool Job website! </P>
-			</LogoDiv>
+			<LogoDiv>{/* <P id={'welcomeText'}>Welcome to the Cool Job website! </P> */}</LogoDiv>
 		</div>
 	);
 };
