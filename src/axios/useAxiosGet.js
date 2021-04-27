761ee0af0b2aa60aa4dc2902b1bc3547415e615c
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-04-27 13:32:07 +0200

    New axios call to handle every get method

---
+++ src/axios/useAxiosGet.js
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const useAxiosGet = (url) => {
+	const [data, setData] = useState([]); //be null
+	const [isLoading, setIsLoading] = useState(true);
+	const [error, setError] = useState(null);
+
+	useEffect(() => {
+		const source = axios.CancelToken.source();
+		axios
+			.get(url, { cancelToken: source.token })
+			.then((resp) => {
+				setData(resp.data);
+				setIsLoading(false);
+			})
+			.catch((err) => {
+				if (axios.isCancel(err)) {
+					console.log('Request is cancelled');
+				} else if (!err.response) {
+					setIsLoading(false);
+					setError('Connection error - no response');
+				} else {
+					if (err.response.status === 404) {
+						setIsLoading(false);
+						setError('404 error - page is not found');
+					}
+				}
+			});
+		return () => {
+			source.cancel('Component got unmounted');
+		};
+	}, [url]);
+
+	return { data, isLoading, error };
+};
+
+export default useAxiosGet;
