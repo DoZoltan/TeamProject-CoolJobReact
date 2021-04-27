c75cf0152fee47b6f37ad8758dcac520735e069f
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-04-27 16:17:44 +0200

    Deleted unused providers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
-import { JobProvider } from './Contexts/JobContext';
-//import { FavoriteJobProvider } from './Contexts/FavoriteJobContext';
-import { OnJobProvider } from './Contexts/OnJobContext';
-//import { FilteredJobProvider } from './Contexts/FilteredJobsContext';
 
+import { OnJobProvider } from './Contexts/OnJobContext';
 import { TheProvider } from './Contexts/TheContext';
 
 import NavBar from './components/navbar/NavBar';
@@ -12,7 +9,6 @@ import Main from './components/mainPage/Main';
 import JobsBox from './components/jobsPage/JobsBox';
 import DetailBox from './components/detailsPage/DetailBox';
 import FavoriteBox from './components/favoritePage/FavoriteBox';
-import JobDetailProvider from './Contexts/JobDetailContext';
 import Add from './components/addAdvertisement/Add';
 import LoginPage from './components/login/LoginPage';
 import RegistrationPage from './components/registration/RegistrationPage';
@@ -21,29 +17,25 @@ const App = () => {
 	return (
 		<TheProvider>
 			<OnJobProvider>
-				<JobDetailProvider>
-					<Router>
-						<JobProvider>
-							<div>
-								<div>
-									<NavBar />
-								</div>
-								<div>
-									<Route exact path='/'>
-										<Redirect to='/main' />
-									</Route>
-									<Route path='/main' component={Main} />
-									<Route path='/jobs' component={JobsBox} />
-									<Route path='/favorite' component={FavoriteBox} />
-									<Route path='/add' component={Add} />
-									<Route path='/detail' component={DetailBox} />
-									<Route path='/login' component={LoginPage} />
-									<Route path='/registration' component={RegistrationPage} />
-								</div>
-							</div>
-						</JobProvider>
-					</Router>
-				</JobDetailProvider>
+				<Router>
+					<div>
+						<div>
+							<NavBar />
+						</div>
+						<div>
+							<Route exact path='/'>
+								<Redirect to='/main' />
+							</Route>
+							<Route path='/main' component={Main} />
+							<Route path='/jobs' component={JobsBox} />
+							<Route path='/favorite' component={FavoriteBox} />
+							<Route path='/add' component={Add} />
+							<Route path='/detail' component={DetailBox} />
+							<Route path='/login' component={LoginPage} />
+							<Route path='/registration' component={RegistrationPage} />
+						</div>
+					</div>
+				</Router>
 			</OnJobProvider>
 		</TheProvider>
 	);
