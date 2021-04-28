31310c8e74ac5bf108088d72cf2441b16a05989f
Roland Horváth <rolandhorvath1025@gmail.com>
2021-04-28 16:40:50 +0200

    Add BackTop

--- src/components/jobsPage/JobsList.js
+++ src/components/jobsPage/JobsList.js
@@ -1,29 +1,32 @@
 import React, { useContext } from 'react';
 import JobCard from './JobCard';
-import { Row } from 'antd';
+import { Row, BackTop } from 'antd';
 import { TheContext } from '../../Contexts/TheContext';
 
 export const JobsList = () => {
 	const { filteredJobs } = useContext(TheContext);
 
 	return (
-		<Row gutter={16}>
-			{filteredJobs.map((job) => (
-				<JobCard
-					key={job.id}
-					id={job.id}
-					type={job.type}
-					created_at={job.created_at}
-					title={job.title}
-					location={job.location}
-					company_logo={job.company_logo}
-					company={job.company}
-					apply={job.how_to_apply}
-					company_url={job.company_url}
-					description={job.description}
-				/>
-			))}
-		</Row>
+		<div>
+			<Row gutter={16}>
+				{filteredJobs.map((job) => (
+					<JobCard
+						key={job.id}
+						id={job.id}
+						type={job.type}
+						created_at={job.created_at}
+						title={job.title}
+						location={job.location}
+						company_logo={job.company_logo}
+						company={job.company}
+						apply={job.how_to_apply}
+						company_url={job.company_url}
+						description={job.description}
+					/>
+				))}
+			</Row>
+			<BackTop />
+		</div>
 	);
 };
 
