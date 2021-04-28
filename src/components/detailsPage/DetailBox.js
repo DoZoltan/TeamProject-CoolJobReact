b01b57559f7933424d7f57bd4d96baeaf039d243
Roland Horváth <rolandhorvath1025@gmail.com>
2021-04-28 16:42:58 +0200

    Put out the styled components from arrow function

--- src/components/detailsPage/DetailBox.js
+++ src/components/detailsPage/DetailBox.js
@@ -4,21 +4,20 @@ import Detail from './Detail';
 import FilterBar from '../filterbar/FilterBar';
 import { BackTop } from 'antd';
 
+const DetailDiv = styled.div`
+	background-color: black;
+`;
+
+const StyledDiv = styled.div`
+	width: 85%;
+	float: right;
+`;
+
+const Div = styled.div`
+	position: fixed;
+	float: left;
+`;
 export const DetailBox = () => {
-	const DetailDiv = styled.div`
-		background-color: black;
-	`;
-
-	const StyledDiv = styled.div`
-		width: 85%;
-		float: right;
-	`;
-
-	const Div = styled.div`
-		position: fixed;
-		float: left;
-	`;
-
 	return (
 		<React.Fragment>
 			<Div>
