5331a22ef5d1271efbad28af58fce1b25192dd75
Roland Horváth <rolandhorvath1025@gmail.com>
2021-05-06 23:02:42 +0200

    Rework RegistrationPage
    Make it responsive

--- src/components/registration/RegistrationPage.js
+++ src/components/registration/RegistrationPage.js
@@ -1,162 +1,253 @@
 import React from 'react';
-import 'antd/dist/antd.css';
-import { Layout, Button } from 'antd';
-import styled from 'styled-components';
-import { useHistory } from 'react-router-dom';
+import Agreement from '../registration/Agreement';
+import { AutoComplete, Form, Input, Select, Row, Col, Checkbox, Button } from 'antd';
+const { Option } = Select;
 
-const Div = styled.div`
-	margin-bottom: 20px;
-`;
+const formItemLayout = {
+	labelCol: {
+		xs: {
+			span: 24,
+		},
+		sm: {
+			span: 8,
+		},
+	},
+	wrapperCol: {
+		xs: {
+			span: 24,
+		},
+		sm: {
+			span: 16,
+		},
+	},
+};
+const tailFormItemLayout = {
+	wrapperCol: {
+		xs: {
+			span: 24,
+			offset: 0,
+		},
+		sm: {
+			span: 16,
+			offset: 8,
+		},
+	},
+};
 
-const Label = styled.label`
-	font-size: 16px;
-	font-weight: bold;
-`;
+const RegistrationPage = () => {
+	const [form] = Form.useForm();
 
-const Input = styled.input`
-	width: 100%;
-	font-size: 14px;
-`;
-
-export const RegistrationPage = () => {
-	var canSend = true;
-	var history = useHistory();
-	const { Footer, Content } = Layout;
-
-	function makeId(length) {
-		var result = '';
-		var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-		var charactersLength = characters.length;
-		for (var i = 0; i < length; i++) {
-			result += characters.charAt(Math.floor(Math.random() * charactersLength));
-		}
-		return result;
-	}
-
-	let resultSample = {
-		userName: '',
-		firstName: '',
-		lastName: '',
-		email: '',
-		birthDate: '',
-		registrationDate: '',
-		password: '',
-		passwordAgain: '',
-		id: '',
+	const onFinish = (values) => {
+		console.log('Received values of form: ', values);
 	};
 
-	let result = {
-		userName: '',
-		firstName: '',
-		lastName: '',
-		email: '',
-		birthDate: '',
-		registrationDate: '',
-		password: '',
-		id: '',
-	};
-
-	function handleChange(event) {
-		resultSample[event.target.id] = event.target.value;
-		document.getElementById(event.target.id).style.border = '1px solid black';
-	}
-
-	function handleSubmit() {
-		const newId = makeId(25);
-
-		var today = new Date();
-		var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-		var time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
-		var fullTime = `${date} ${time}`;
-
-		resultSample['id'] = newId;
-		resultSample['registrationDate'] = fullTime;
-
-		for (const [key, value] of Object.entries(resultSample)) {
-			if (value.length < 1) {
-				document.getElementById(key).style.border = '1px solid red';
-				canSend = false;
-			}
-
-			if (key === 'passwordAgain' && resultSample['password'] !== value) {
-				document.getElementById(key).style.border = '1px solid red';
-				canSend = false;
-			}
-		}
-		if (canSend === true) {
-			// PostApiData(result, 'https://localhost:44318/api/Jobs');
-			// window.location.reload(false);
-			history.push('/login');
-		} else {
-			canSend = true;
-			updateResult();
-		}
-	}
-
-	function updateResult() {
-		for (const [key] of Object.entries(result)) {
-			result[key] = resultSample[key];
-		}
-	}
+	const prefixSelector = (
+		<Form.Item name='prefix' noStyle>
+			<Select
+				style={{
+					width: 70,
+				}}
+			>
+				<Option value='36'>+36</Option>
+			</Select>
+		</Form.Item>
+	);
 
 	return (
-		<Layout>
-			<Content style={{ marginLeft: 250, marginRight: 250 }}>
-				<form method='post'>
-					<fieldset>
-						<legend>
-							<h3 style={{ marginTop: 25 }}>Registration</h3>
-						</legend>
-						<Div>
-							<Label htmlFor='userName'>user name:</Label>
-							<br />
-							<Input type='text' id='userName' onChange={handleChange} />
-						</Div>
+		<div className='block registrationBlock'>
+			<div className='titleHolderLogin'>
+				<h2>Registration</h2>
+				<p>Give your data for successful registration!</p>
+			</div>
+			<Form
+				{...formItemLayout}
+				form={form}
+				name='register'
+				onFinish={onFinish}
+				initialValues={{
+					residence: ['zhejiang', 'hangzhou', 'xihu'],
+					prefix: '+36',
+				}}
+				scrollToFirstError
+			>
+				<Form.Item
+					name='email'
+					label='E-mail'
+					rules={[
+						{
+							type: 'email',
+							message: 'The input is not valid E-mail!',
+						},
+						{
+							required: true,
+							message: 'Please input your E-mail!',
+						},
+					]}
+				>
+					<Input />
+				</Form.Item>
 
-						<Div>
-							<Label htmlFor='firstName'>first name:</Label>
-							<br />
-							<Input type='text' id='firstName' onChange={handleChange} />
-						</Div>
+				<Form.Item
+					name='password'
+					label='Password'
+					rules={[
+						{
+							required: true,
+							message: 'Please input your password!',
+						},
+					]}
+					hasFeedback
+				>
+					<Input.Password />
+				</Form.Item>
 
-						<Div>
-							<Label htmlFor='lastName'>last name:</Label>
-							<br />
-							<Input type='text' id='lastName' onChange={handleChange} />
-						</Div>
+				<Form.Item
+					name='confirm'
+					label='Confirm Password'
+					dependencies={['password']}
+					hasFeedback
+					rules={[
+						{
+							required: true,
+							message: 'Please confirm your password!',
+						},
+						({ getFieldValue }) => ({
+							validator(_, value) {
+								if (!value || getFieldValue('password') === value) {
+									return Promise.resolve();
+								}
 
-						<Div>
-							<Label htmlFor='birthDate'>birth date:</Label>
-							<br />
-							<Input type='text' id='birthDate' onChange={handleChange} />
-						</Div>
+								return Promise.reject(
+									new Error('The two passwords that you entered do not match!')
+								);
+							},
+						}),
+					]}
+				>
+					<Input.Password />
+				</Form.Item>
 
-						<Div>
-							<Label htmlFor='email'>email</Label>
-							<br />
-							<Input type='text' id='email' onChange={handleChange} />
-						</Div>
+				<Form.Item
+					name='nickname'
+					label='Nickname'
+					tooltip='What do you want others to call you?'
+					rules={[
+						{
+							required: true,
+							message: 'Please input your nickname!',
+							whitespace: true,
+						},
+					]}
+				>
+					<Input />
+				</Form.Item>
+				<Form.Item
+					name='country'
+					label='Country'
+					rules={[
+						{
+							required: true,
+							message: 'Please input your Country!',
+						},
+					]}
+				>
+					<Input />
+				</Form.Item>
+				<Form.Item
+					name='city'
+					label='Actual city'
+					rules={[
+						{
+							required: true,
+							message: 'Please select your habitual residence!',
+						},
+					]}
+				>
+					{/* <AutoComplete
+						value={inputLocationValue}
+						id={'locationFilter'}
+						dropdownClassName='certain-category-search-dropdown'
+						dropdownMatchSelectWidth={300}
+						style={{
+							width: 180,
+						}}
+						options={optionsLocation}
+						filterOption={(inputValue, option) =>
+							option.value.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1
+						}
+						onChange={changeCardListByLocation}
+					>
+						<Input.Search id={'locationInput'} size='large' placeholder='Location' />
+					</AutoComplete> */}
+					<Input />
+				</Form.Item>
 
-						<Div>
-							<Label htmlFor='password'>password:</Label>
-							<br />
-							<Input type='password' id='password' onChange={handleChange} />
-						</Div>
+				<Form.Item
+					name='phone'
+					label='Phone Number'
+					rules={[
+						{
+							required: true,
+							message: 'Please input your phone number!',
+						},
+					]}
+				>
+					<Input
+						addonBefore={prefixSelector}
+						style={{
+							width: '100%',
+						}}
+					/>
+				</Form.Item>
 
-						<Div>
-							<Label htmlFor='passwordAgain'>password again:</Label>
-							<br />
-							<Input type='password' id='passwordAgain' onChange={handleChange} />
-						</Div>
-					</fieldset>
-				</form>
-			</Content>
-			<Footer style={{ alignSelf: 'center' }}>
-				<Button id={'submitButton'} style={{ fontSize: 18 }} onClick={handleSubmit}>
-					Registration
-				</Button>
-			</Footer>
-		</Layout>
+				<Form.Item label='Captcha' extra='We must make sure that your are a human.'>
+					<Row gutter={8}>
+						<Col span={12}>
+							<Form.Item
+								name='captcha'
+								noStyle
+								rules={[
+									{
+										required: true,
+										message: 'Please input the captcha you got!',
+									},
+								]}
+							>
+								<Input />
+							</Form.Item>
+						</Col>
+						<Col span={12}>
+							<Button>Get captcha</Button>
+						</Col>
+					</Row>
+				</Form.Item>
+
+				<Form.Item
+					name='agreementCheck'
+					valuePropName='checked'
+					rules={[
+						{
+							validator: (_, value) =>
+								value
+									? Promise.resolve()
+									: Promise.reject(new Error('Should accept agreement')),
+						},
+					]}
+					{...tailFormItemLayout}
+				>
+					<Checkbox>
+						<p>
+							I have read the <Agreement />
+						</p>
+					</Checkbox>
+				</Form.Item>
+				<Form.Item {...tailFormItemLayout}>
+					<Button type='primary' htmlType='submit'>
+						Register
+					</Button>
+				</Form.Item>
+			</Form>
+		</div>
 	);
 };
 
