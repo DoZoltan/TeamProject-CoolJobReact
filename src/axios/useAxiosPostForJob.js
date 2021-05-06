3d54e7e87684f3b7f73f487540d0d7a93e327bfb
Roland Horváth <rolandhorvath1025@gmail.com>
2021-05-06 23:05:21 +0200

    Change userID to userId

--- src/axios/useAxiosPostForJob.js
+++ src/axios/useAxiosPostForJob.js
@@ -4,7 +4,7 @@ export const UseAxiosPostForJob = (job, _url) => {
 	let data = JSON.stringify(job);
 	axios.post(_url, data, {
 		headers: { 'Content-Type': 'application/json' },
-		params: { userID: 2 },
+		params: { userId: 2 },
 	});
 };
 
