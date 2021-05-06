822439b1e25e23f257d9d915fa0df81dd02717e8
Roland Horváth <rolandhorvath1025@gmail.com>
2021-05-06 02:51:11 +0200

    Change style

--- src/components/mainPage/Main.js
+++ src/components/mainPage/Main.js
@@ -20,8 +20,11 @@ const LogoDiv = styled.div`
 
 export const Main = () => {
 	return (
-		<div>
-			<LogoDiv>{/* <P id={'welcomeText'}>Welcome to the Cool Job website! </P> */}</LogoDiv>
+		<div className='main'>
+			<video src='/videos/p.mp4' autoPlay loop muted />
+			<h1>JOB ADVENTURE AWAITS</h1>
+			<p>Welcome to the Cool Job website!</p>
+			{/* <LogoDiv><P id={'welcomeText'}>Welcome to the Cool Job website! </P></LogoDiv> */}
 		</div>
 	);
 };
