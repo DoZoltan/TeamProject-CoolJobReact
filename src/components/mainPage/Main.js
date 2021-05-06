c2c02645aa31a286ecb6bfadef0de09032c58ac7
Roland Horváth <rolandhorvath1025@gmail.com>
2021-05-06 23:07:45 +0200

    Delete styled.div

--- src/components/mainPage/Main.js
+++ src/components/mainPage/Main.js
@@ -1,22 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
-import background from '../../resources/background.png';
-
-const LogoDiv = styled.div`
-	background-size: cover;
-	background-position: center center;
-	background-repeat: no-repeat;
-	background-image: url(${background});
-	width: 100%;
-	height: 100vh;
-`;
-
-// const P = styled.p`
-// 	color: orange;
-// 	padding: 20px;
-// 	text-align: center;
-// 	font-size: 3.5vw;
-// `;
 
 export const Main = () => {
 	return (
