5c3466bf317e16f1aea724381dadb6b2ba80e5a4
Roland Horváth <rolandhorvath1025@gmail.com>
2021-05-10 16:53:49 +0200

    Add textContainer div.
    Start to use popconfirm button from ant design

--- src/components/detailsPage/Detail.js
+++ src/components/detailsPage/Detail.js
@@ -7,13 +7,14 @@ import { useHistory } from 'react-router-dom';
 import UseSimpleGetAxios from '../../axios/useSimpleGetAxios';
 import { UserContext } from '../../Contexts/UserContext';
 
-
 const Detail = (props) => {
 	const { Header, Footer, Content } = Layout;
 	const [successDisplay, setSuccessDisplay] = useState(false);
 	const [successDeleteDisplay, setSuccessDeleteDisplay] = useState(false);
 	const [existInFavorite, setExistInFavorite] = useState(false);
 	const { user } = useContext(UserContext);
+	const [visible, setVisible] = React.useState(false);
+	const [confirmLoading, setConfirmLoading] = React.useState(false);
 
 	useEffect(() => {
 		// The user is a simple number now
@@ -31,6 +32,23 @@ const Detail = (props) => {
 		};
 		JobExistInFavorite();
 	}, [props.details.id, user]);
+
+	const showPopconfirm = () => {
+		setVisible(true);
+	};
+
+	const handleOk = () => {
+		setConfirmLoading(true);
+		setTimeout(() => {
+			setVisible(false);
+			setConfirmLoading(false);
+		}, 2000);
+	};
+
+	const handleCancel = () => {
+		console.log('Clicked cancel button');
+		setVisible(false);
+	};
 
 	const AddJobToFavoriteList = () => {
 		UseAxiosPostForJob(props.details, 'https://localhost:44318/api/Favorites');
@@ -78,7 +96,7 @@ const Detail = (props) => {
 
 	return (
 		<Layout>
-			<Header id={'header'} style={{ color: '#F5FFFA', backgroundColor: '#000080' }}>
+			<Header id={'detailHeader'}>
 				{/* <Link to={history.go(-1)}> */}
 				{/* <Link to={onJob === true ? '/jobs' : '/favorite'}> */}
 				<Button
@@ -95,85 +113,101 @@ const Detail = (props) => {
 				</Button>
 				{/* </Link> */}
 				<h1 id={'headerTitle'} style={{ color: '#F5FFFA' }}>
-					{props.details.title}
+					{props.details.company}
 				</h1>
 			</Header>
-			<Content id={'content'}>
-				<div style={{ paddingRight: '400px', paddingLeft: '400px' }}>
-					<Image id={'imgLogo'} src={detail.company_logo} />
-				</div>
-				<div id={'textBox'} style={{ margin: '20px' }}>
-					<h2 id={'companyName'}>Company name: {props.details.company}</h2>
-					<h3 id={'jobType'}>Job title: {props.details.type}</h3>
-					<h3 id={'jobLocation'}>Job location: {props.details.location}</h3>
-					<h4 id={'companyPage'}>
-						company page:{' '}
-						<a href={props.details.company_url}>{props.details.company_url}</a>
-					</h4>
-					<p
-						id={'descriptionParagraph'}
-						dangerouslySetInnerHTML={{ __html: props.details.apply }}
+			<div id={'textContainer'}>
+				<Content id={'content'}>
+					<div id={'imgContainer'}>
+						<Image className={'imgLogo'} src={props.details.company_logo} />
+					</div>
+					<div id={'textBox'}>
+						<h2 id={'jobTitleBox'}>
+							Job title: <p id={'jobTitle'}> {props.details.title}</p>
+						</h2>
+						<h3 id={'jobType'}>Job type: {props.details.type}</h3>
+						<h3 id={'jobLocation'}>Job location: {props.details.location}</h3>
+						<h4 id={'companyPage'}>
+							company page: <a href={props.details.company_url}>Link</a>
+						</h4>
+						<p
+							id={'applyParagraph'}
+							dangerouslySetInnerHTML={{ __html: props.details.apply }}
+						/>
+						<h4 id={'jobDescription'}>descritpiton:</h4>
+						<p dangerouslySetInnerHTML={{ __html: props.details.description }} />
+					</div>
+					<Alert
+						id={'successAlert'}
+						style={SuccessAlertStyle}
+						message='Added to favorite jobs'
+						type='success'
+						showIcon
 					/>
-					<h4 id={'jobDescription'}>descritpiton:</h4>
-					<p dangerouslySetInnerHTML={{ __html: props.details.description }} />
-				</div>
-				<Alert
-					id={'successAlert'}
-					style={SuccessAlertStyle}
-					message='Added to favorite jobs'
-					type='success'
-					showIcon
-				/>
-				<Alert
-					id={'deleteAlert'}
-					style={SuccessDeleteAlertStyle}
-					message='Deleted from favorite jobs'
-					type='error'
-					showIcon
-				/>
-			</Content>
-			<Footer id={'detailFooter'}>
-				Added at : {props.details.created_at}
-				<Button
-					id={'addToFavoriteButton'}
-					style={{
-						color: 'white',
-						backgroundColor: '#2F4F4F',
-						marginRight: '140px',
-						float: 'right',
-						display: `${
-							existInFavorite === false && successDisplay === false ? 'block' : 'none'
-						}`,
-					}}
-					onClick={() => {
-						alertTimeoutForAddSuccess();
-						setSuccessDisplay(true);
-						AddJobToFavoriteList();
-						setExistInFavorite(true);
-					}}
-				>
-					Add to favorites
-				</Button>
-				<Button
-					id={'deleteFromFavoriteButton'}
-					style={{
-						color: 'white',
-						backgroundColor: '#2F4F4F',
-						marginRight: '140px',
-						float: 'right',
+					<Alert
+						id={'deleteAlert'}
+						style={SuccessDeleteAlertStyle}
+						message='Deleted from favorite jobs'
+						type='error'
+						showIcon
+					/>
+				</Content>
+				<Footer id={'detailFooter'} style={{ padding: '0px' }}>
+					Added at : {props.details.created_at}
+					<Button
+						id={'addToFavoriteButton'}
+						style={{
+							color: 'white',
+							backgroundColor: '#2F4F4F',
 
-						display: `${existInFavorite === true ? 'block' : 'none'}`,
-					}}
-					onClick={() => {
-						alertTimeoutForDeleteSuccess();
-						setSuccessDeleteDisplay(true);
-						DeleteJobFromFavoriteList();
-						setExistInFavorite(false);
-					}}
+							float: 'right',
+							display: `${
+								existInFavorite === false && successDisplay === false
+									? 'block'
+									: 'none'
+							}`,
+						}}
+						onClick={() => {
+							alertTimeoutForAddSuccess();
+							setSuccessDisplay(true);
+							AddJobToFavoriteList();
+							setExistInFavorite(true);
+						}}
+					>
+						Add to favorites
+					</Button>
+					{/* <Popconfirm
+					title='Title'
+					visible={visible}
+					onConfirm={handleOk}
+					okButtonProps={{ loading: confirmLoading }}
+					onCancel={handleCancel}
 				>
-					Delete from favorites
-				</Button>
-			</Footer>
+					<Button type='primary' onClick={showPopconfirm}>
+						Open Popconfirm with async logic
+					</Button>
+				</Popconfirm> */}
+					<Button
+						id={'deleteFromFavoriteButton'}
+						style={{
+							color: 'white',
+							backgroundColor: '#2F4F4F',
+
+							float: 'right',
+
+							display: `${existInFavorite === true ? 'block' : 'none'}`,
+						}}
+						onClick={() => {
+							alertTimeoutForDeleteSuccess();
+							setSuccessDeleteDisplay(true);
+							DeleteJobFromFavoriteList();
+							setExistInFavorite(false);
+						}}
+					>
+						Delete from favorites
+					</Button>
+				</Footer>
+			</div>
 		</Layout>
 	);
 };
