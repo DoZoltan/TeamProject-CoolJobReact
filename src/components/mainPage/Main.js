9bb45e4902881de431641d9f7fe7521eca97c64f
Roland Horváth <rolandhorvath1025@gmail.com>
2021-05-11 13:52:31 +0200

    Add id for h1

--- src/components/mainPage/Main.js
+++ src/components/mainPage/Main.js
@@ -4,7 +4,7 @@ const Main = () => {
 	return (
 		<div className='main'>
 			<video src='/videos/p.mp4' autoPlay loop muted />
-			<h1>JOB ADVENTURE AWAITS</h1>
+			<h1 id={"mainHead"}>JOB ADVENTURE AWAITS</h1>
 			<p>Welcome to the Cool Job website!</p>
 			{/* <LogoDiv><P id={'welcomeText'}>Welcome to the Cool Job website! </P></LogoDiv> */}
 		</div>
